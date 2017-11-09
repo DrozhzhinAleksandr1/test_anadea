7e28be2f09d13ef18b8e6357479fdc22cc001915
DrozhzhinAleksandr1 <DrozhzhinAleksandr1@gmail.com>
2017-11-09 23:15:52 +0200

    create select and add some styles

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,27 @@
 window.onload = function(){
-
+	var select = document.getElementsByClassName('cardGameSize')[0],
+		containerForCards = document.getElementsByClassName('containerForCards')[0];
+		buildCardGame(6);
 	
+	select.addEventListener('change', function(){
+		var selectValue = +select.value;
+		buildCardGame(selectValue);
+	});
+
+	function appendDiv(number){
+		var div = document.createElement('div');
+		div.className = 'containerForCards__cards containerForCards__cards_number' + number;
+		containerForCards.appendChild(div);
+	}
+
+	function buildCardGame(number){
+		containerForCards.innerHTML = '';
+
+		for(var i = 1; i<=(number*number);i++){
+			appendDiv(number);
+		}
+			
+	}
 }
+
+
